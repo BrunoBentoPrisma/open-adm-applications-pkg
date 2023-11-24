0485e5ddf491d2c463b4a7ce2c15648c75a79848
Bruno <brunobento@prismafive.com.br>
2023-11-24 17:29:37 -0300

    fix

---
+++ Test/Test/Services/CachedServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using application_pkg.Services;
+using Bogus;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+
+namespace Test.Test.Services;
+
+public class CachedServiceTest
+{
+    private readonly Faker _faker;
+    private readonly Mock<IDistributedCache> _mock;
+    private readonly CachedService<string> _cachedService;
+
+    public CachedServiceTest()
+    {
+        _faker = new();
+        _mock = new();
+        _cachedService = new(_mock.Object);
+    }
+    //[Fact]
+    //public async Task DeveSetarCached()
+    //{
+    //    var key = _faker.Lorem.Text();
+    //    var value = _faker.Lorem.Paragraph();
+    //    _mock.Setup(x => x.SetStringAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(true));
+    //    var result = await _cachedService.SetItemAsync(key, value);
+    //    Assert.True(result);
+    //}
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task NaoDeveSetarCachedComKeyInvalida(string key)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(() => _cachedService.SetItemAsync(key, _faker.Lorem.Paragraph()));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task NaoDeveSetarCollectionComKeyInvalida(string key)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(() => _cachedService.SetCollectionAsync(key,new() { _faker.Lorem.Paragraph(), _faker.Lorem.Paragraph() }));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task NaoDeveRetornarItemCachedComKeyInvalida(string key)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(() => _cachedService.GetItemAsync(key));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task NaoDeveRetornarCollectionCachedComKeyInvalida(string key)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(() => _cachedService.GetCollectionAsync(key));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    public async Task NaoDeveSetarCachedComValueNull(string? value)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(() => _cachedService.SetItemAsync(_faker.Lorem.Paragraph(), value));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task NaoDeveRemoverCachedComKeyInvalida(string key)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(() => _cachedService.RemoveCached(key));
+    }
+}
--- application-pkg/Services/CachedService.cs
+++ application-pkg/Services/CachedService.cs
@@ -15,43 +15,54 @@ public class CachedService<T> : ICachedService<T> where T : class
 
     public async Task<List<T>?> GetCollectionAsync(string key)
     {
+        ValidateKey(key);
         var values = await _distributedCache.GetStringAsync(key);
         return values is null ? null : JsonSerializer.Deserialize<List<T>>(values);
     }
 
     public async Task<T?> GetItemAsync(string key)
     {
+        ValidateKey(key);
         var value = await _distributedCache.GetStringAsync(key);
         return value is null ? null : JsonSerializer.Deserialize<T>(value);
     }
 
-    public async Task RemoveCached(string key) => await _distributedCache.RemoveAsync(key);
+    public async Task RemoveCached(string key)
+    {
+        ValidateKey(key);
+        await _distributedCache.RemoveAsync(key);
+    }
 
     public async Task<bool> SetCollectionAsync(string key, List<T> values)
     {
-        try
-        {
-            var valuesJson = JsonSerializer.Serialize<List<T>>(values);
-            await _distributedCache.SetStringAsync(key, valuesJson, _options);
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        ValidateKey(key);
+        ValidateListValueNull(values);
+        var valuesJson = JsonSerializer.Serialize<List<T>>(values);
+        await _distributedCache.SetStringAsync(key, valuesJson, _options);
+        return true;
     }
 
     public async Task<bool> SetItemAsync(string key, T value)
     {
-        try
-        {
-            var valueJson = JsonSerializer.Serialize<T>(value);
-            await _distributedCache.SetStringAsync(key, valueJson, _options);
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
+        ValidateKey(key);
+        ValidateValueNull(value);
+        var valueJson = JsonSerializer.Serialize<T>(value);
+        await _distributedCache.SetStringAsync(key, valueJson, _options);
+        return true;
+    }
+
+    private static void ValidateKey(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key)) throw new ArgumentException("key inválida!");
+    }
+
+    private static void ValidateValueNull(T? value)
+    {
+        if (value == null) throw new ArgumentException("key inválida!");
+    }
+
+    private static void ValidateListValueNull(List<T>? value)
+    {
+        if (value == null) throw new ArgumentException("key inválida!");
     }
 }
